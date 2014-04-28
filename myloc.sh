2914af9d7e39e07b14f8d6880c9aa7b32a278aa0
Venkata Sai Sriram Mahavadi <sriram.mahavadi@gmail.com>
2014-04-29 01:49:06 +0530

    LOC modified to count in deprecated code lines.

--- myloc.sh
+++ myloc.sh
@@ -6,14 +6,17 @@
 # Created on 7 Apr, 2014, 8:33:35 AM
 #
 FILES=./local/*
+DEPRECATED_FILES=./local/deprecated/*
+FILES="$FILES $DEPRECATED_FILES"
+echo $DEPRECATED_FILES
 TOTAL_LOC=0
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 #echo "Inital lines: $TOTAL_LOC"
 echo "******************* LOC ****************"
-printf " %-30s $GREEN[%10s]$NORMAL\n" "File Name" "LOC"
-printf " %-30s   $GREEN%10s$NORMAL\n" "==================" "========= "
+printf " %-50s $GREEN[%10s]$NORMAL\n" "File Name" "LOC"
+printf " %-50s   $GREEN%10s$NORMAL\n" "==================" "========= "
 for f in $FILES
 do
 
@@ -24,7 +27,7 @@ if [ -f "${f}" ] ; then
   LOC=$(wc -l < "$f")
   #$LOC := wc -l $f
   #echo "Number of lines in file: $f is: $LOC"
-  printf " %-30s $GREEN[%10d]$NORMAL\n" $f $LOC
+  printf " %-50s $GREEN[%10d]$NORMAL\n" $f $LOC
   TOTAL_LOC=$(($TOTAL_LOC + $LOC))
 fi
 done
