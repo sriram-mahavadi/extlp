5dcb2ae1f1208c22bf5d0b0cae2c54a00f56d570
Sriram Mahavadi <sriram.mahavadi@gmail.com>
2014-06-05 21:04:29 +0530

    Scripts for simple execution and server upload

---
+++ lp_accuracy_checker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 
+# File:   lp_accuracy_checker.sh
+# Author: Sriram Mahavadi
+#
+# Created on 5 Jun, 2014, 11:39:10 AM
+#
+
+SOPLEX_EXECUTABLE='/home/harsha/Desktop/sriram_thesis/softwares/soplex-2.0.0.linux.x86_64.gnu.opt'
+EXTLP_EXECUTABLE='/home/harsha/Desktop/sriram_thesis/codebase/extlp/build/local/extlp'
+
+FILES='/home/
